3b85815f483a73d6f9d251d75569eb93d5843f93
Matheus Gomes <matheusgomes192@hotmail.com>
2021-01-19 19:29:35 -0300

    Criando e quase terminando o conteúdo de cima (Button para assinar premium e link com regras de uso)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import Logo from './img/logo_spotify.png';
+import User from './img/user_img.jpeg';
+import Dropdown from './img/dropdown.png';
+
 import {
     Container,
 
@@ -10,35 +14,67 @@ import {
     UserImg,
     UserText,
     DropdownImg,
+
+    ContentTop,
+    ContentTopText,
+    ContentTopLink,
 } from './AppStyled'
 
-import Logo from './img/logo_spotify.png';
-import User from './img/user_img.jpeg';
-import Dropdown from './img/dropdown.png';
+import {Button} from '@material-ui/core';
+
+import {
+    withStyles,
+} from '@material-ui/core/styles';
 
 
 export default () => {
+    const DefaultBtn = withStyles(() => ({
+        root: {
+            backgroundColor: '#5DA443',
+            color: '#fff',
+            height: 40,
+            width: 400,
+            borderRadius: 20,
+            marginTop: 45,
+            textAlign: 'center',
+            fontWeight: 'bold',
+            textDecoration: 'none',
+
+
+            '&:hover': {
+                backgroundColor: '#38D45F',
+            }
+        },
+    }))(Button);
+
     return (
         <Container>
             <Header>
                 <HeaderLogo src={Logo}/>
 
                 <HeaderRight>
-                    <HeaderLink>Premium</HeaderLink>
-                    <HeaderLink>Suporte</HeaderLink>
-                    <HeaderLink>Baixar</HeaderLink>
+                    <HeaderLink href={"#"}>Premium</HeaderLink>
+                    <HeaderLink href={"#"}>Suporte</HeaderLink>
+                    <HeaderLink href={"#"}>Baixar</HeaderLink>
 
                     <LineDiv></LineDiv>
 
-                    <UserLink>
+                    <UserLink href={"#"}>
                         <UserImg src={User}/>
-                        <UserText>Perfil</UserText>
+                        Perfil
                         <DropdownImg src={Dropdown}/>
                     </UserLink>
                 </HeaderRight>
 
-
             </Header>
+
+            <ContentTop>
+                <ContentTopText>Vá de Premium. E seja feliz!</ContentTopText>
+
+                <DefaultBtn href={"#"}>OBTER SPOTIFY PREMIUM</DefaultBtn>
+
+                <ContentTopLink href={"#"}>*Pode obter regras de uso</ContentTopLink>
+            </ContentTop>
         </Container>
     );
 }
--- src/AppStyled.js
+++ src/AppStyled.js
@@ -9,6 +9,7 @@ export const Header = styled.div`
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
+    position: fixed;
 `;
 export const HeaderLogo = styled.img`
     width: 135px;
@@ -26,6 +27,11 @@ export const HeaderLink = styled.a`
     color: #fff;
     font-weight: bold;
     margin-right: 32px;
+    text-decoration: none;
+    
+    :hover {
+        color: #38D45F;
+    }
 `;
 export const LineDiv = styled.div`
     height: 15px;
@@ -38,20 +44,43 @@ export const UserLink = styled.a`
     display: flex;
     flex-direction: row;
     align-items: center;
+    color: #fff;
+    font-weight: bold;
+    text-decoration: none;
+    
+    :hover {
+        color: #38D45F;
+    }
 `;
 export const UserImg = styled.img`
     width: 40px;
     height: 40px;
     border-radius: 20px;
-`;
-export const UserText = styled.span`
-    color: #fff;
-    font-weight: bold;
-    margin-left: 15px;
+    margin-right: 15px;
 `;
 export const DropdownImg = styled.img`
     width: 15px;
     height: 15px;
     margin: 5px 0 0 10px;
+`;
 
+
+export const ContentTop = styled.div`
+    height: 640px;
+    width: 100%;
+    background-color: #F2B155;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+export const ContentTopText = styled.span`
+    color: #fff;
+    font-size: 80px;
+    font-weight: bold;
+    text-align: center;
+    width: 80%;
+`;
+export const ContentTopLink = styled.a`
+    color: #eee;
 `;
